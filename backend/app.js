835eae09d474dd680e0bc5b55735a2ed50387c6c
Rahul Jha <rahulbudy11@gmail.com>
2025-05-05 18:57:01 +0530

    set up folder structure for controllers and routes

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
+const authRoutes = require('./routes/authRoutes')
+
 
 const sequelize = require('./config/sql');
 const mongo = require('./config/mongo')
+
+app.use('/api/auth/', authRoutes)
 
 
 sequelize.sync();
